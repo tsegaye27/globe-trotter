b8fbbe8ea3ed46f3e0714f414a9d7ef4704408ba
tsegaye27 <ttalegn29@gmail.com>
2024-04-04 21:46:43 +0300

    Refactored authentication context and add login/logout functionality

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -3,39 +3,54 @@ import { useContext, createContext, useReducer } from "react";
 const AuthContext = createContext();
 
 const initialState = {
-  user: {},
-  isLoading: false,
-  isLoggedIn: false,
-  error: "",
+  user: null,
+  isAuthenticated: false,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "LOGIN_LOADING":
-      return { ...state, isLoading: true };
-    case "LOGIN_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        isLoggedIn: true,
-        user: action.payload,
-      };
-    case "LOGIN_ERROR":
-      return { ...state, isLoading: false, error: action.payload };
+    case "login":
+      return { ...state, isAuthenticated: true, user: action.payload };
+    case "logout":
+      return { ...state, isAuthenticated: false, user: null };
     default:
       throw new Error("Unknown action type");
   }
 }
 
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside of AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
