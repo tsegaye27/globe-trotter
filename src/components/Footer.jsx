5386be731d51b00a06650243595a5da6b971ef06
tsegaye27 <ttalegn29@gmail.com>
2025-03-13 09:12:58 +0300

    refactor: rename project from WorldWise to GlobeTrotter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.copyright}>
-        &copy; Copyright {new Date().getFullYear()} WorldWise Inc.
+        &copy; Copyright {new Date().getFullYear()} GlobeTrotter Inc.
       </p>
     </footer>
   );
